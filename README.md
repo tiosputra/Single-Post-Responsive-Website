01bb196cad78ab28573741ca7f09181b47eb6ce4
Tio Saputra <karya.tiosaputra@gmail.com>
2018-04-10 08:38:55 +0700

    Updating navbar

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Single-Post-Responsive-Website
 This is a project for learning : SCSS, Responsive Web design, and media query
+
+Not done yet
+ - add "Resize navbar when scrool"
+ - modify the footer (adding something usefull, maybe social media, or contact me link)
+ - navbar cant expand if we scrool (need to be clicked twice)
+
+
+Improvement Logs
+April 10 2018
+- _nav.scss already mobile first
+- modfy navbar to the better version
+- already putting this website to git
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,19 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function toggleNavbar() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+    if (x.className === "nav") {
+        x.className += " nav-responsive";
     } else {
-        x.className = "topnav";
+        x.className = "nav";
     }
 }
+
+$(window).scroll(function() {
+    if($(document).scrollTop() > 50) {
+        $('.nav').addClass('shrink');
+        console.log("Adding class");
+    }else {
+        $('.nav').removeClass('shrink');
+        console.log("Removing class");
+    }
+})
